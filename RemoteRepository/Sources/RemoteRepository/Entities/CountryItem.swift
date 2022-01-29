f8a421a8e477a17cab2191df05c4c62ffafa4977
Amir Ardalani <amir.ardalanuk@gmail.com>
2022-01-29 16:58:49 +0330

    Add `CountryItem` entity on RemoteRepository module

---
+++ RemoteRepository/Sources/RemoteRepository/Entities/CountryItem.swift
@@ -0,0 +1,47 @@
+//
+//  CountryItem.swift
+//  
+//
+//  Created by Amir Ardalani on 1/29/22.
+//
+
+import Foundation
+
+struct CountryItem: Decodable {
+    let name: String
+    let region: String
+    let flag: String
+    let capital: [String]
+    let area: Int
+    
+    enum RootKey: String, CodingKey {
+        case name
+        case region
+        case flag
+        case capital
+        case area
+    }
+    
+    enum NameKey: String, CodingKey {
+        case official
+        case common
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: RootKey.self)
+        self.flag = try container.decode(String.self, forKey: .flag)
+        self.region = try container.decode(String.self, forKey: .region)
+        self.area = try container.decode(Int.self, forKey: .area)
+        
+        var capitalContainer = try container.nestedUnkeyedContainer(forKey: .capital)
+        var capitalList = [String]()
+        while !capitalContainer.isAtEnd {
+            let capital = try capitalContainer.decode(String.self)
+            capitalList.append(capital)
+        }
+        self.capital = capitalList
+        
+        let nameContainer = try container.nestedContainer(keyedBy: NameKey.self, forKey: .name)
+        self.name = try nameContainer.decode(String.self, forKey: .common)
+    }
+}
