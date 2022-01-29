82d1ee39585c6b8d80cc68785a9ba4baa9d7efa9
Amir Ardalani <amir.ardalanuk@gmail.com>
2022-01-29 15:49:39 +0330

    Add `Core` and `HTTPClient` module as Dependency  to the RemoteRepository

--- RemoteRepository/Package.swift
+++ RemoteRepository/Package.swift
@@ -6,18 +6,15 @@ import PackageDescription
 let package = Package(
     name: "RemoteRepository",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RemoteRepository",
             targets: ["RemoteRepository"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Core", path: "../Core"),
+        .package(name: "HTTPClient", path: "../HTTPClient")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RemoteRepository",
             dependencies: []),
