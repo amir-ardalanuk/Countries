147ab2f9b254cb956571ce93b34d0b1c40cd7a1e
Amir Ardalani <amir.ardalanuk@gmail.com>
2022-01-29 13:12:52 +0330

    Add MockCountryListUsecase

---
+++ Core/Sources/Core/Usecases/__mock/MockCountryUsecase.swift
@@ -0,0 +1,26 @@
+#if DEBUG
+//
+//  File.swift
+//  
+//
+//  Created by Amir Ardalani on 1/29/22.
+//
+
+import Foundation
+
+public final class MockCountryListUsecases: CountryUsecase {
+
+    public init() {}
+    
+    public var resultProvider: (() -> Result<[Country], CountryUsecaseError>)?
+    
+    public func fetchCountryList(completion: CountryListCompletion) {
+        if let result = resultProvider?() {
+            completion(result)
+        }
+        completion(.success([.stub()]))
+    }
+
+}
+
+#endif
