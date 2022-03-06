d4888b7f7e18db15d11102d847d2412c12e5ec58
Amir <amir.ardalanuk@gmail.com>
2022-03-06 10:47:40 +0330

    Add 'openNewsDetail' to the `NewsListRouting`

--- ChopeNews/ChopeNews/Scene/NewsList/NewsListRouter.swift
+++ ChopeNews/ChopeNews/Scene/NewsList/NewsListRouter.swift
@@ -8,8 +8,15 @@
 import Foundation
 import UIKit
 
-protocol NewsListRouting: Router {}
+protocol NewsListRouting: Router {
+    func openNewsDetail(configuration: NewsDetail.Configuration)
+}
 
 final class NewsListRouter: NewsListRouting {
     weak var viewController: UIViewController?
+    
+    func openNewsDetail(configuration: NewsDetail.Configuration) {
+        let newsDetail = NewsDetailModule().makeScene(configuration: configuration)
+        viewController?.navigationController?.pushViewController(newsDetail, animated: true)
+    }
 }
